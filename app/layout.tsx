fa6a4000e96e54503b6a00d53d1ad62125d9a215
olegsamoilenko <soniac@ukr.net>
2026-03-23 12:53:16 +0200

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { manrope, prata } from '@/lib/fonts'
 import GlobalToaster from '@/components/ui/GlobalToaster'
+import Script from 'next/script'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -12,6 +13,8 @@ const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
 })
+
+const GA_ID = 'G-6GHVVK4BB3'
 
 export default function RootLayout({
   children,
@@ -30,6 +33,19 @@ export default function RootLayout({
         <GlobalToaster />
         {children}
       </body>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          window.gtag = gtag;
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
     </html>
   )
 }
